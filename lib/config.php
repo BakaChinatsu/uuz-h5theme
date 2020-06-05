1731680de6c82a575631427c43603253dbd510e0
雨宮千夏 <9513654+BakaChinatsu@users.noreply.github.com>
2020-06-05 18:00:46 +0900

    Update to fontawesome 5.13.0
    Update new version from html5up and fontawesome.
    Fix issues and add Japanese translate.

--- lib/config.php
+++ lib/config.php
@@ -1,56 +1,69 @@
 <?php
 /**
  * uuz-h5theme配置文件
+ * uuz-h5theme設定ファイル
  * Created by PhpStorm.
  * User: Amamiya Chinatsu's
  * Date: 2016/3/5
  * Time: 12:22
  */
-//定义网站标题
+
+//请将图片文件放入 /images 文件夹
+//画像ファイルを /images に入れてください
+
+//html_lang
+$html_lang = "ja";
+//定义网站标题 //サイトのタイトル
 $title = "雨宮千夏の博麗神社";
 
-//定义Chrome theme-color
+//网站图标 //タグに表示するアイコンの設定
+$tag_icon = "icon.jpg";
+
+//定义 theme-color //ブラウザ要素の色
 $theme_color = "#22849f";
 
-//设置背景图像
-//请将背景文件放入 /images 文件夹
+//设置背景图像 //背景の設定
 $background_filename = "50327795_p0.jpg";
 
-//设置头像
-//请将头像文件放入 /images 文件夹
+//设置头像 //アバターの設定
 $avatar_filename = "reimu.jpg";
+$avatar_height = "130px"; //高度//高さ
+$avatar_width = "130px"; //宽度 //幅
 
-//设置主标题
+//设置主标题 //表題の設定
 $main_title = "雨宮千夏";
 
-//设置二级标题
-$sec_title = "永遠とはそういうこと。ワビの世界よ。";
+//设置二级标题 //副題の設定
+$sec_title = "永遠とはそういうこと<br>ワビの世界よ";
 
-//设置连接地址
-//如需设置其他图标，请参考 http://fontawesome.io/icons/
-//设置第一个图标及替代文字
-$icons_01 = "fa-twitter";
-$txt_01 = "Twitter";
-$url_01 = "https://twitter.com/BakaChinatsu/";
+//设置链接及图标 //アイコン及びURLの設定
+//如需设置其他图标，请参考 fontawesome.io/icons/ //ほかのアイコンを設定したい場合は fontawesome.io/icons/ に参照してください
+//设置第一个图标、替代文字及链接 //一番目のアイコン、テキスト及びURLの設定
+$icon_style_01 = "brands"; //图标风格(Style)，具体参考 fontawesome.io/icons/ //アイコンの様式(Style) fontawesome.io/icons/ に参照してください
+$site_icon_01 = "fa-twitter";
+$site_name_01 = "Twitter";
+$site_url_01 = "https://twitter.com/BakaChinatsu/";
 
-//设置第二个图标及替代文字
-$icons_02 = "fa-github";
-$txt_02 = "GitHub";
-$url_02 = "https://github.com/BakaChinatsu/";
+//设置第二个图标及替代文字 //二番目のアイコン、テキスト及びURLの設定
+$icon_style_02 = "brands"; //图标风格(Style)，具体参考 fontawesome.io/icons/ //アイコンの様式(Style) fontawesome.io/icons/ に参照してください
+$site_icon_02 = "fa-github";
+$site_name_02 = "GitHub";
+$site_url_02 = "https://github.com/BakaChinatsu/";
 
-//设置第三个图标及替代文字(永远怀念 Google+ ）
-//$icons_03 = "fa-google-plus";
-//$txt_03 = "Google+";
-//$url_03 = "https://plus.google.com/u/0/106072901155470558440";
-$icons_03 = "fa-camera-retro";
-$txt_03 = "Photos";
-$url_03 = "https://photos.app.goo.gl/CLZQrE0LrK5dSVFE2";
+//设置第三个图标及替代文字(永远怀念 Google+ ） //三番目のアイコン、テキスト及びURLの設定
+//$site_icons_03 = "fa-google-plus";
+//$site_txt_03 = "Google+";
+//$site_url_03 = "https://plus.google.com/u/0/106072901155470558440";
+$icon_style_03 = "solid"; //图标风格(Style)，具体参考 http://fontawesome.io/icons/ //アイコンの様式(Style) http://fontawesome.io/icons/ に参照してください
+$site_icon_03 = "fa-camera-retro";
+$site_name_03 = "Photos";
+$site_url_03 = "https://photos.app.goo.gl/CLZQrE0LrK5dSVFE2";
 
-//设置下方 button 信息
+//设置下方 button 信息 //アイコン下側のボタンの設定
 $button_name = "Blog";
 $button_url = "https://blog.uuz.moe";
 
-//设置版权信息
+//设置版权信息 //Copyrightの設定
 $copyright = "Amamiya Chinatsu";
 ?>
 
